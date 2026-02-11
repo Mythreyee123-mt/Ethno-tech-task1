6e84251d937a02060851422bbde4c8464c41b1ad
Mythreyee123-mt <mtmythreyee@gmail.com>
2026-02-11 10:27:07 +0530

    Add Student class with display method

---
+++ pg.java
@@ -0,0 +1,19 @@
+public class Student {
+	String name;   
+    int age;
+
+    void display() {   
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student();  
+
+        s1.name = "Mythreyee";  
+        s1.age = 20;
+
+        s1.display();  
+    }
+}
